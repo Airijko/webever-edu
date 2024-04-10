8446f3769a6420ca705ff9a0c4169ff618c5c73a
Airijko <airijkoblade@gmail.com>
2024-04-10 12:26:00 -0500

    improve mobile ui

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ img, title, content }) => {
     return (
-        <div className="flex flex-row md:flex-col justify-center items-center border rounded-xl p-10 w-full h-full md:h-[300px] md:w-[300px] gap-2.5">
+        <div className="flex flex-col min-[500px]:flex-row md:flex-col justify-center items-center border rounded-xl p-10 w-full h-full md:h-[300px] md:w-[300px] gap-2.5">
             <img src={img} alt="Logo" className="w-[80px]" />
             <h1 className="text-xl">
                 {title}
