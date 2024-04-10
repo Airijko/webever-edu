f0d4219f1063241e3fb06cda34fbd32c5089f3f6
Airijko <airijkoblade@gmail.com>
2024-04-10 12:06:34 -0500

    fix mobile view

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -7,13 +7,15 @@ const MainNav = () => {
         <nav className="flex flex-row px-10 py-5 w-full z-10">
             <div className='flex flex-row items-center w-full'>
                 <img src={Logo} alt="Logo" className="w-auto h-[50px]" />
-                <div className="flex flex-row justify-end gap-7.5 text-sm uppercase flex-grow">
-                    <div href="/" className="">Home</div>
-                    <div href="/" className="">Services</div>
-                    <div href="/" className="">Training</div>
-                    <div href="/" className="">Inquiry</div>
-                    <div href="/" className="">About us</div>
-                    <div href="/" className="">Contact</div>
+                <div className="hidden lg:block flex-grow">
+                    <div className="flex flex-row justify-end gap-7.5 text-sm uppercase flex-grow">
+                        <div href="/" className="">Home</div>
+                        <div href="/" className="">Services</div>
+                        <div href="/" className="">Training</div>
+                        <div href="/" className="">Inquiry</div>
+                        <div href="/" className="">About us</div>
+                        <div href="/" className="">Contact</div>
+                    </div>
                 </div>
             </div>
         </nav>
